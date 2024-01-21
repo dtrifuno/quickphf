3e8113a4fc5a17b03ca3c071b324317b76f75cb9
Nikolai Vazquez <hello@nikolaivazquez.com>
2024-01-21 00:03:38 -0500

    Use runtime arguments in Divan benchmarks
    This greatly reduces compile times and is not limited to arrays/slices.

--- benchmarks/benches/generate.rs
+++ benchmarks/benches/generate.rs
@@ -6,21 +6,21 @@ use benchmarks::SIZES;
 
 const SEED: u64 = 42;
 
-#[divan::bench(consts = SIZES, max_time = 10)]
-fn quickphf<const S: usize>(bencher: divan::Bencher) {
+#[divan::bench(args = SIZES, max_time = 10)]
+fn quickphf(bencher: divan::Bencher, size: usize) {
     let mut rng = fastrand::Rng::with_seed(SEED);
 
     bencher
-        .with_inputs(|| repeat_with(|| rng.u64(..)).take(S).collect())
+        .with_inputs(|| repeat_with(|| rng.u64(..)).take(size).collect())
         .bench_local_refs(|keys: &mut Vec<u64>| generate_phf(keys));
 }
 
-#[divan::bench(consts = SIZES, max_time = 10)]
-fn phf<const S: usize>(bencher: divan::Bencher) {
+#[divan::bench(args = SIZES, max_time = 10)]
+fn phf(bencher: divan::Bencher, size: usize) {
     let mut rng = fastrand::Rng::with_seed(SEED);
 
     bencher
-        .with_inputs(|| repeat_with(|| rng.u64(..)).take(S).collect())
+        .with_inputs(|| repeat_with(|| rng.u64(..)).take(size).collect())
         .bench_local_refs(|keys: &mut Vec<u64>| phf_generator::generate_hash(keys));
 }
 
